121bccfcbd3150124fe876f2a8ecdd56fe8b9287
MarcC <mcoquand@gmail.com>
2023-05-03 20:48:03 +0000

    add colombia theme

---
+++ Colombia.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides', ["#1f1f1e", "#f44336", "#5b9d5e", "#5b9d5e", "#2e237c", "#7f7480", "#398bc6", "#8b8792", "#655f6d", "#aa573c", "#26232a", "#585260", "#7e7887", "#e2dfe7", "#bf40bf", "#fff5e6"]);
+t.prefs_.set('foreground-color', "#1F1F1E");
+t.prefs_.set('background-color', "#FFF5e6");
+t.prefs_.set('cursor-color', '#7F9088');
